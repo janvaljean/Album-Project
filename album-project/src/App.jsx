fa92c892b0dcafdcd83ed37b74445a47e1470d58
janvaljean <kurtuldu@gmx.de>
2023-04-20 00:24:57 +0200

    v1 project installed

---
+++ album-project/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import UsersList from './components/UsersList'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+     <UsersList/>
+    </div>
+  )
+}
+
+export default App
---
+++ album-project/src/components/UsersList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UsersList = () => {
+  return (
+    <div>
+      USerlist
+    </div>
+  )
+}
+
+export default UsersList
