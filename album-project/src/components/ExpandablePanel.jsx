d5856efb58bdde104f4df6c593a41c569f429321
janvaljean <kurtuldu@gmx.de>
2023-04-22 18:01:57 +0200

    v7 GoChevronDown and left, setExpanded

--- album-project/src/components/ExpandablePanel.jsx
+++ album-project/src/components/ExpandablePanel.jsx
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { GoChevronLeft } from "react-icons/go";
 import { GoChevronDown } from "react-icons/go";
 
 const ExpandablePanel = ({ header, children }) => {
+  const [expanded, setExpanded] = useState(false)
+  const handleClick = () => {
+    setExpanded(!expanded)
+  }
 
   return (
     <div className="panelDiv">
       <div className="topArrangement">
         <div className="topArrangement">{header}</div>
-        <div>
-          <GoChevronLeft />
+        <div onClick={handleClick}>
+          {expanded ? <GoChevronDown /> : <GoChevronLeft />}
         </div>
       </div>
-
-      {children}
+      {expanded && <div>{children}</div>}
     </div>
   );
 };
